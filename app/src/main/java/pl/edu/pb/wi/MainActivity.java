6e7cc2bd5ee2aa9a6d8e4c8a7c3df3c204db6c54
M.Pietruczuk <102554956+mpietruczuk07@users.noreply.github.com>
2022-10-12 18:48:16 +0200

    initial commit

--- app/src/main/java/pl/edu/pb/wi/MainActivity.java
+++ app/src/main/java/pl/edu/pb/wi/MainActivity.java
@@ -1,28 +1,54 @@
 package pl.edu.pb.wi;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String KEY_EXTRA_ANSWER = "pl.edu.pb.wi.quiz.correctAnswer";
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    private static boolean answerWasShown;
+    private static final int REQUEST_CODE_PROMPT = 0;
+
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
+    private Button promptButton;
     private TextView questionTextView;
+
+    private Question[] questions = new Question[]{
+            new Question(R.string.q_activity, true),
+            new Question(R.string.q_find_resources, false),
+            new Question(R.string.q_listener, true),
+            new Question(R.string.q_resources, true),
+            new Question(R.string.q_version, false),
+    };
+
+    private int currentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("activity", "Wywołana została metoda: onCreate");
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null){
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
 
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
+        promptButton = findViewById(R.id.tip_button);
         questionTextView = findViewById(R.id.question_text_view);
+
 
         trueButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -42,30 +68,85 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 currentIndex = (currentIndex + 1)%questions.length;
+                answerWasShown = false;
                 setNextQuestion();
             }
         });
+
+        promptButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+                boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+                intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
+                startActivityForResult(intent, REQUEST_CODE_PROMPT);
+            }
+        });
+
         setNextQuestion();
     }
 
-    private Question[] questions = new Question[]{
-            new Question(R.string.q_activity, true),
-            new Question(R.string.q_find_resources, false),
-            new Question(R.string.q_listener, true),
-            new Question(R.string.q_resources, true),
-            new Question(R.string.q_version, false),
-    };
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("activity", "Wywołana została metoda: onStart");
+    }
 
-    private int currentIndex = 0;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("activity", "Wywołana została metoda: onResume");
+    }
 
-    private void checkAnswerCorrectness(boolean userAnswer){
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("activity", "Wywołana została metoda: onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("activity", "Wywołana została metoda: onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("activity", "Wywołana została metoda: onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        Log.d("activity", "Wywołana została metoda: onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_CODE_PROMPT){
+            if (data == null) {
+                return;
+            }
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+        }
+    }
+
+    private void checkAnswerCorrectness(boolean userAnswer) {
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
         int resultMessageId = 0;
-        if(userAnswer == correctAnswer){
-            resultMessageId = R.string.correct_answer;
-        }
-        else{
-            resultMessageId = R.string.incorrect_answer;
+        if (answerWasShown) {
+            resultMessageId = R.string.answer_was_shown;
+        } else {
+            if (userAnswer == correctAnswer) {
+                resultMessageId = R.string.correct_answer;
+            } else {
+                resultMessageId = R.string.incorrect_answer;
+            }
         }
         Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
     }
